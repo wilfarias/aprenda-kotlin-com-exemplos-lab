26f4ce7f21010d607207359afb2743f83e345747
Wilson Farias <69092681+wilfarias@users.noreply.github.com>
2023-10-24 22:45:13 -0300

    Implementacao do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,66 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String, val idade: Int, val email: String){
+    override fun toString(): String {
+        return "$nome"
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+fun UsuarioFactory(): List<Usuario>{    
+    val listaUsuarios = mutableListOf<Usuario>()
+    
+    listaUsuarios.add(Usuario("Carlos", 19, "carlos@gmail.com"))
+    listaUsuarios.add(Usuario("Andre", 24, "andre@gmail.com"))
+    listaUsuarios.add(Usuario("Julia", 28, "julia@gmail.com"))
+    listaUsuarios.add(Usuario("Yuri", 20, "yuri@gmail.com"))
+    listaUsuarios.add(Usuario("Luisa", 32, "luisa@gmail.com"))
+    
+    return listaUsuarios
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(val nome: String, val cargaHoraria: Int, val nivel: Nivel)
+
+fun ConteudoEducacionalFactory(): List<ConteudoEducacional>{    
+    val listaMaterias = mutableListOf<ConteudoEducacional>()
+    
+    listaMaterias.add(ConteudoEducacional("Introdução a linguagem", 20, Nivel.BASICO))
+    listaMaterias.add(ConteudoEducacional("Estruturas de Controle de Fluxo", 40, Nivel.BASICO))
+    listaMaterias.add(ConteudoEducacional("Orientação a Objetos", 60, Nivel.BASICO))
+    listaMaterias.add(ConteudoEducacional("Tratamento de Exceções", 60, Nivel.INTERMEDIARIO))
+    listaMaterias.add(ConteudoEducacional("Padrões de Projeto", 80, Nivel.INTERMEDIARIO))
+    
+    return listaMaterias
+}
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    
+    fun matricularTodos(listaUsuarios: List<Usuario>) {
+        inscritos.addAll(listaUsuarios)
+    }
+    
+    fun imprimeAlunosMatriculados(){ 
+        inscritos.forEach {                                     
+        	aluno -> println("Aluno: $aluno matriculado")
+    	}
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val formacaoKotlin = Formacao("Formação Kotlin", ConteudoEducacionalFactory(), Nivel.BASICO)  
+    formacaoKotlin.matricularTodos(UsuarioFactory())    
+    formacaoKotlin.imprimeAlunosMatriculados()
+    
+    formacaoKotlin.matricular(Usuario("Jorge", 42, "jorge@gmail.com"))
+    println()
+    formacaoKotlin.imprimeAlunosMatriculados()
+  
 }
